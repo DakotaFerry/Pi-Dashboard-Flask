291fdee5459f4dca6eb9ded8a28f593d9b08feba
inteuniso <dzferry9@gmail.com>
2020-01-28 16:30:11 -0600

    Laziness

---
+++ dockerInstall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
+
+# Install Docker
+# The aufs package, part of the "recommended" packages, won't install on Buster just yet, because of missing pre-compiled kernel modules.
+# We can work around that issue by using "--no-install-recommends"
+sudo apt update
+sudo apt install -y --no-install-recommends \
+    docker-ce \
+    cgroupfs-mount
